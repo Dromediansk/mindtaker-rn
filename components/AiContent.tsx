88062637e69d0656de39950ec0b95daf137b6a93
Dromediansk <pillar.mr@gmail.com>
2025-07-03 11:52:07 +0200

    markdown ai content

--- components/AiContent.tsx
+++ components/AiContent.tsx
@@ -1,6 +1,6 @@
 import { ScrollView } from "react-native";
 import React, { FC } from "react";
-import { StyledText } from "./StyledText";
+import Markdown from "react-native-markdown-display";
 
 type AiContentProps = {
   aiResponse: string;
@@ -10,10 +10,31 @@ const AiContent: FC<AiContentProps> = ({ aiResponse }) => {
   if (!aiResponse) return null;
 
   return (
-    <ScrollView className="max-h-80 bg-gray-100 rounded-lg m-2 border-2 border-main">
-      <StyledText className="p-5 text-xl font-workSans-light">
+    <ScrollView className="max-h-96 bg-gray-100 rounded-lg m-2 border-2 border-main">
+      <Markdown
+        style={{
+          body: {
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            fontSize: 20,
+            fontFamily: "WorkSans-Light",
+            color: "#000",
+          },
+          paragraph: {
+            fontSize: 20,
+            lineHeight: 28,
+            marginBottom: 10,
+          },
+          strong: {
+            fontWeight: "bold",
+          },
+          em: {
+            fontStyle: "italic",
+          },
+        }}
+      >
         {aiResponse}
-      </StyledText>
+      </Markdown>
     </ScrollView>
   );
 };
